55c29cb8deba6b452d6a298001ac8a30198ff9c4
Demo-159 <davidmonrroy7@gmail.com>
2025-06-22 21:28:54 -0400

    Create cart_provider.dart

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+import '../models/product.dart';
+import '../models/cart_item.dart';
+
+class CartProvider with ChangeNotifier {
+  List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+  
+  int get itemCount => _items.fold(0, (sum, item) => sum + item.quantity);
+  
+  double get totalAmount => _items.fold(0.0, (sum, item) => sum + item.totalPrice);
+
+  void addItem(Product product) {
+    final existingIndex = _items.indexWhere((item) => item.product.id == product.id);
+    
+    if (existingIndex >= 0) {
+      _items[existingIndex].quantity++;
+    } else {
+      _items.add(CartItem(product: product));
+    }
+    notifyListeners();
+  }
+
+  void removeItem(int index) {
+    if (index >= 0 && index < _items.length) {
+      if (_items[index].quantity > 1) {
+        _items[index].quantity--;
+      } else {
+        _items.removeAt(index);
+      }
+      notifyListeners();
+    }
+  }
+
+  void clearCart() {
+    _items.clear();
+    notifyListeners();
+  }
+
+  void updateQuantity(int index, int newQuantity) {
+    if (index >= 0 && index < _items.length && newQuantity > 0) {
+      _items[index].quantity = newQuantity;
+      notifyListeners();
+    }
+  }
+}
